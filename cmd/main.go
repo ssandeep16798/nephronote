484fdcbb1e089b3a832d02c6b05138840351edbb
ssandeep16798 <sandeepmanda98@yahoo.com>
2024-05-22 20:40:39 +0530

    refactored code to handle middleware and save pre-dialysis data.

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"nephronote/internal/handlers"
+	"nephronote/middleware"
 	"net/http"
 
 	_ "github.com/lib/pq"
@@ -11,23 +12,23 @@ import (
 )
 
 func main() {
+	r := mux.NewRouter()
 
-	r := mux.NewRouter() // Add parentheses to call the function
-
+	// Public routes
 	r.HandleFunc("/registeruser/", handlers.Register).Methods("POST")
 	r.HandleFunc("/login/", handlers.Login).Methods("GET")
+
+	// Protected routes
+	r.PathPrefix("/api/").Subrouter()
+	r.Use(middleware.AuthMiddleware)
 	r.HandleFunc("/pre_dialysis/", handlers.PreDialysisHandler).Methods("POST")
 	r.HandleFunc("/post_dialysis/", handlers.PostDialysisHandler).Methods("POST")
-	/*r.HandleFunc("/healthdata/", func(w http.ResponseWriter, r *http.Request) {
-		// Call the TrackerAPI handler function and pass the database connection
-		handler := handlers.TrackerAPI(db)
-		// Call the handler function, which writes the response directly to the ResponseWriter
-		handler(w, r)
-	}).Methods("POST")*/
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: r,
 	}
+
 	log.Println("Initializing server at localhost", server.Addr, "....")
 	if err := server.ListenAndServe(); err != nil {
 		log.Println("Error starting server:", err)
